a8d5056811aab740b4e709ec04e7b5a5e9c1f79c
Anthony Brown <abrown4123@gmail.com>
2023-09-19 22:12:21 -0500

    Add error handling and notice for invalid file type

--- src/App.js
+++ src/App.js
@@ -7,14 +7,23 @@ function App() {
 
   const [code, setCode] = useState([])
   const [jsonLog, setJsonLog] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
   
   const handleChange = e => {
     const fileReader = new FileReader();
     fileReader.readAsText(e.target.files[0], "UTF-8");
     fileReader.onload = e => {
-      console.log("e.target.result", e.target.result);
-      setJsonLog(JSON.parse(e.target.result));
+      try {
+        console.log("e.target.result", JSON.parse(e.target.result));
+        setJsonLog(JSON.parse(e.target.result));
+      } catch(e) {
+          console.log(e)
+          setErrorMessage(`The uploaded file is not valid JSON`);
+          setTimeout(() => {
+            setErrorMessage(null);
+          }, 5000)
+      }
     };
   }
 
@@ -49,8 +58,9 @@ function App() {
     <div className="App">
       <h1>Command Converter</h1>
       <h3>This app reverse engineers Sauce Selenium log.json files to create simple test scripts</h3>
+      <h4 className="error">{errorMessage}</h4>
       <div>
-        <input id="json" type="file" name="json" onChange={handleChange} />
+        <input id="json" type="file" name="json" accept="json" onChange={handleChange} />
         <button type="submit" onClick={convertCode}>
           Convert!
         </button>
