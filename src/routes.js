a514171402489796cfaaf667aeeac9da0019c4bd
Anthony Brown <abrown4123@gmail.com>
2023-09-25 22:39:51 -0500

    Improve findElements command support and sendKeys value flexibility

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,11 @@ const locatorStrategy = request => {
   if(request.using.includes("xpath")) return "xpath"
 }
 
+const getTextValue = request => {
+  if (request.text !== undefined) return request.text;
+  return request.value.join('');
+}
+
 const getIdFromPath = path => path.split("/")[1]
 
 const getElementId = result => {
@@ -13,7 +18,12 @@ const getElementId = result => {
   }
 }
 
-const findElemResults = (testCommands, result, i) => {
+const findElementsResults = (testCommands, result, i) => {
+  result.forEach((value, iterator) => testCommands[Object.values(value)[0]] = `elements${i}[${iterator}]`);
+  return '';
+}
+
+const findElementResults = (testCommands, result, i) => {
   if(result.length === 0) return '';
   if(result.error !== undefined) return ` // command returned ${result.error}`;
   if(result.message !== undefined) return ` // command returned ${result.message}`;
@@ -22,17 +32,20 @@ const findElemResults = (testCommands, result, i) => {
 }
 
 const routePath = (path, method, request, result, testCommands, i) => {
+  let splitPath = path.split("/")
+  let route = splitPath[splitPath.length-1]
   if (method === "POST" && path === "url") return pythonMappings[method][path](request.url) //This path is driver.get
   
   if (method === "POST" && path.includes("click")) {
     return pythonMappings[method]["click"](testCommands[getIdFromPath(path)]) //this is click
   }
 
-  if (method === "POST" && path.includes("value")) return pythonMappings[method]["sendKeys"](testCommands[getIdFromPath(path)], request.text) //this is sendKeys
+  if (method === "POST" && path.includes("value")) return pythonMappings[method]["sendKeys"](testCommands[getIdFromPath(path)], getTextValue(request)) //this is sendKeys
   
-  if (method === "POST" && (path === "element" || path === "elements")) { //This path is findElem
+  if (method === "POST" && (route === "element" || route === "elements")) { //This path is findElem
     let strategy = locatorStrategy(request)
-    return `${pythonMappings[method][path][strategy](`elem${i}`, request.value)}${findElemResults(testCommands, result, i)}`
+    if(route === "element") return `${pythonMappings[method][route][strategy](`elem${i}`, request.value)}${findElementResults(testCommands, result, i)}`
+    if(route === "elements") return `${pythonMappings[method][route][strategy](`elements${i}`, request.value)}${findElementsResults(testCommands, result, i)}`
   }
 
   if (pythonMappings[method][path] !== undefined) return pythonMappings[method][path] //for get methods that do not need an argument
